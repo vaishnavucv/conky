1a0ab3992825dd25cf3b5902e9a2d5a37842419e
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2020-09-03 10:44:43 +0000

    scorit updated

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
-sudo apt install -y conky
 
+chmod +x *
+
+sudo apt install -y conky
 
 sudo  mkdir -p ~/.config/autostart
 
+sudo mv conky.desktop /root
 
-sudo mv .config/autostart/conky.desktop
+sudo  rm -rf /etc/conky
 
+sudo mv conky /etc/
 
-sudo mv /home/wolfy/.config/autostart 
+cd
 
+sudo mv conky.desktop .config/autostart
 
 sudo reboot
